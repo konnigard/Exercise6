d7ee53239f9a74dfc06e49a53e424722340450e1
Baldur25 <baldur25@ru.is>
2025-09-05 10:04:34 +0000

    Gerði skemmtilegra forrit

--- baldur_1.py
+++ baldur_1.py
@@ -1,1 +1,12 @@
-print("Tihihihi")
+import random
+rand1 = random.randint(0, 100)
+rand2 = random.randint(0,100)
+
+while True:
+    if rand1 + rand2 == 69:
+        print(f"{rand1} + {rand2} is 69")
+        break
+    rand1 = random.randint(0, 100)
+    rand2 = random.randint(0,100)
+
+
