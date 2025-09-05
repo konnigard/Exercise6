382deb4771a103ae9efd8bd6e2a49ca258bdbb69
Baldur25 <baldur25@ru.is>
2025-09-05 09:40:28 +0000

    Breytti einu >>>>>>:DD

--- Hakon_1.py
+++ Hakon_1.py
@@ -1,1 +1,9 @@
-print("Hallo Heimur")
+import random
+
+print("BLESSSSSSS Heimur")
+
+while True:
+    rand1 = random.randint(0, 1000000)
+    print("HAHAHHAHA")
+    if rand1 == 123465:
+        break
