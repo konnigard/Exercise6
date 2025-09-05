bd5d287f7e25113cd0d654c41d6f3e26aa4b9d12
Baldur25 <baldur25@ru.is>
2025-09-05 09:54:12 +0000

    Lét þetta keyra í aðeins styttri tíma

--- Hakon_1.py
+++ Hakon_1.py
@@ -3,7 +3,8 @@ import random
 print("BLESSSSSSS Heimur")
 
 while True:
-    rand1 = random.randint(0, 1000000)
+    rand1 = random.randint(0, 100)
     print("HAHAHHAHA")
-    if rand1 == 123465:
+    if rand1 == 69:
+        print("Ok ég nenni þessu ekki lengur")
         break
