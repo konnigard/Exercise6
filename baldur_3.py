cbae7fb99475d595ec823d6102d1dc170be6b3cd
Baldur25 <baldur25@ru.is>
2025-09-05 09:17:35 +0000

    Adding my files

---
+++ baldur_3.py
@@ -0,0 +1,8 @@
+while True:
+    break
+for i in range(1000,1000):
+    i = i
+print(end="")
+n = input("Veldu heiltölu frá -100 og upp í 100 og ég mun giska hana >:)")
+print(f"{n}")
+print("hehe")
